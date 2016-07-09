7e430bffaa75ecee55783fcec980b188a28aaf8e
haniokasai♡ <htek@haniokasai.com>
2016-07-09 12:01:34 +0200

    Add files via upload

---
+++ src/main/java/com/haniokasai/nukkit/SPAMBAN/Main.java
@@ -0,0 +1,46 @@
+package com.haniokasai.nukkit.SPAMBAN;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.plugin.PluginBase;
+
+
+
+public class Main extends PluginBase implements Listener{
+	static Map<String, Integer> lc = new HashMap<String, Integer>();
+	static Map<String, Integer> spam = new HashMap<String, Integer>();
+	static int ctime =(int) (System.currentTimeMillis()/1000);
+
+
+	public void onEnable() {
+		 this.getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onmotion(PlayerChatEvent event){
+		Player player = event.getPlayer();
+		String name   = player.getName();
+		if(lc.containsKey(name)){
+			if(ctime - lc.get(name)<=5){
+				int i = spam.get(name)+1;
+				spam.put(name,i);
+			}else{
+				spam.put(name,0);
+			}
+			if(spam.get(name)>15){
+				String reason = " [SPAMBAN] "+name+" was banned.";
+				String ip = player.getAddress();
+				Server.getInstance().broadcastMessage(" [SPAMBAN] "+name+" was banned."+ip);
+				player.getServer().getIPBans().addBan(ip,"ChatBan", null,reason);//ip-ban
+				player.getServer().getNameBans().addBan(name,"ChatBan", null,reason);//ban
+			}
+		}
+		lc.put(name,ctime);
+	}
+}
